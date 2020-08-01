04c99adc2fd93d7927ea2e9293ce1f8ddc69657f
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-01 16:43:31 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @songs = Artist.find(params[:artist_id]).songs 
     else 
-      @songs = Song.all
+      redirect to @song 
     end
   end
 
